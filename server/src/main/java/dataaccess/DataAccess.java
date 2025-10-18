4a37e3cbfcd228798dcc9eb6d2458994fe4b46fc
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-10-17 19:43:01 -0600

    Add doc comments to DataAccess and MemoryDataAccess methods

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -3,7 +3,20 @@ package dataaccess;
 import model.UserData;
 
 public interface DataAccess {
+    /**
+     * Insert data for a new user into the data store.
+     *
+     * @param userData The user data to insert.
+     * @throws DataAccessException If the user data was unable to be inserted into the data store.
+     */
     void insertUser(UserData userData) throws DataAccessException;
 
+    /**
+     * Get data for an existing user from the data store.
+     *
+     * @param username The username of the user whose data is to be retrieved.
+     * @return A UserData object corresponding to the given username.
+     * @throws DataAccessException If the user data was unable to be retrieved from the data store.
+     */
     UserData getUser(String username) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -7,11 +7,17 @@ import java.util.HashMap;
 public class MemoryDataAccess implements DataAccess {
     private final HashMap<String, UserData> users = new HashMap<>();
 
+    /**
+     * Insert data for a new user into the data store.
+     *
+     * @param userData The user data to insert.
+     * @throws DataAccessException If there is already user data in the data store with the given username.
+     */
     @Override
     public void insertUser(UserData userData) throws DataAccessException {
         String username = userData.username();
         var result = users.put(username, userData);
-        
+
         if (result != null) {
             throw new DataAccessException(
                     "Tried to register a new user with username " + username + ", but there is already a user registered with that name"
@@ -19,6 +25,13 @@ public class MemoryDataAccess implements DataAccess {
         }
     }
 
+    /**
+     * Get data for an existing user from the data store.
+     *
+     * @param username The username of the user whose data is to be retrieved.
+     * @return A UserData object corresponding to the given username.
+     * @throws DataAccessException If the user data does not exist in the data store.
+     */
     @Override
     public UserData getUser(String username) throws DataAccessException {
         UserData userData = users.get(username);
