cf0cdf35e835b3be478e7e976b6c150ae3ad4d4f
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-10-22 20:31:48 -0600

    Write positive and negative test for UserService

---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,53 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.EntryAlreadyExistsException;
+import dataaccess.EntryNotFoundException;
+import dataaccess.MemoryDataAccess;
+import model.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void registerNewUser() throws EntryNotFoundException {
+        DataAccess dataAccess = new MemoryDataAccess();
+        AuthService authService = new AuthService();
+        UserService userService = new UserService(authService, dataAccess);
+
+        UserData user = new UserData(
+                "bob_java",
+                "kotlingoblin",
+                "bjava@jmail.com"
+        );
+
+        // Make sure user is not in database
+        assertThrows(EntryNotFoundException.class, () -> dataAccess.getUser(user.username()));
+
+        // register user
+        assertDoesNotThrow(() -> userService.register(user));
+
+        // Assert user is in database
+        UserData savedUser = dataAccess.getUser(user.username());
+
+        assertEquals(user, savedUser);
+    }
+
+    @Test
+    void usernameAlreadyTaken() {
+        DataAccess dataAccess = new MemoryDataAccess();
+        AuthService authService = new AuthService();
+        UserService userService = new UserService(authService, dataAccess);
+
+        UserData user = new UserData(
+                "bob_java",
+                "kotlingoblin",
+                "bjava@jmail.com"
+        );
+
+        assertDoesNotThrow(() -> userService.register(user));
+        assertThrows(EntryAlreadyExistsException.class, () -> userService.register(user));
+    }
+}
