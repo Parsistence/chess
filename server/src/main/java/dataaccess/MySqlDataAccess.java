d50b11037cd0a288b1c0cbb6918f21679020ae92
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-11-01 21:37:26 -0600

    Create boilerplate code for MySqlDataAccess

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,143 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MySqlDataAccess implements DataAccess {
+    /**
+     * Clears all user data in the database.
+     */
+    @Override
+    public void clearUsers() {
+
+    }
+
+    /**
+     * Clears all auth data in the database.
+     */
+    @Override
+    public void clearAuthData() {
+
+    }
+
+    /**
+     * Clears all game data in the database.
+     */
+    @Override
+    public void clearGameData() {
+
+    }
+
+    /**
+     * Insert data for a new user into the data store.
+     *
+     * @param userData The user data to insert.
+     * @throws EntryAlreadyExistsException If the user data was unable to be inserted into the data store.
+     */
+    @Override
+    public void insertUser(UserData userData) throws EntryAlreadyExistsException {
+
+    }
+
+    /**
+     * Get data for an existing user from the data store.
+     *
+     * @param username The username of the user whose data is to be retrieved.
+     * @return A UserData object corresponding to the given username.
+     * @throws EntryNotFoundException If the user data was unable to be retrieved from the data store.
+     */
+    @Override
+    public UserData getUser(String username) throws EntryNotFoundException {
+        return null;
+    }
+
+    /**
+     * Insert new auth data into the database
+     *
+     * @param authData The auth data to insert
+     * @throws EntryAlreadyExistsException If auth data already exists in database
+     */
+    @Override
+    public void insertAuthData(AuthData authData) throws EntryAlreadyExistsException {
+
+    }
+
+    /**
+     * Get auth data from the database.
+     *
+     * @param authToken The auth token corresponding to the auth data.
+     * @return The auth data.
+     */
+    @Override
+    public AuthData getAuthData(String authToken) throws EntryNotFoundException {
+        return null;
+    }
+
+    /**
+     * Remove auth data from the database.
+     *
+     * @param authToken the auth token associated with the auth data.
+     */
+    @Override
+    public void removeAuth(String authToken) {
+
+    }
+
+    /**
+     * Gets all games in the database.
+     *
+     * @return A collection of all games in the database.
+     */
+    @Override
+    public Collection<GameData> listGames() {
+        return List.of();
+    }
+
+    /**
+     * Creates a new game and adds it to the database.
+     *
+     * @param gameName The name to give the new game.
+     * @return The game data added to the database.
+     */
+    @Override
+    public GameData createGame(String gameName) throws EntryAlreadyExistsException {
+        return null;
+    }
+
+    /**
+     * Gets a user in the database given the user's auth token.
+     *
+     * @param authToken The auth token associated with the user.
+     * @return The user data.
+     */
+    @Override
+    public UserData getUserFromAuth(String authToken) throws EntryNotFoundException {
+        return null;
+    }
+
+    /**
+     * Gets a game from the database given a game ID.
+     *
+     * @param gameID The game ID associated with the game.
+     * @return The game.
+     */
+    @Override
+    public GameData getGame(int gameID) throws EntryNotFoundException {
+        return null;
+    }
+
+    /**
+     * Updates game data in the database with the given data.
+     *
+     * @param gameID      The game ID.
+     * @param updatedGame The data to update into the database.
+     */
+    @Override
+    public void updateGame(int gameID, GameData updatedGame) throws EntryNotFoundException {
+
+    }
+}
