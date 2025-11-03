68f93797e9b59506d53ecd57bf1cf4fd85afbeb5
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-11-03 16:30:33 -0700

    Add additional constructors for EntryAlreadyExistsException

--- server/src/main/java/dataaccess/EntryAlreadyExistsException.java
+++ server/src/main/java/dataaccess/EntryAlreadyExistsException.java
@@ -4,4 +4,12 @@ public class EntryAlreadyExistsException extends DataAccessException {
     public EntryAlreadyExistsException(String message) {
         super(message);
     }
+
+    public EntryAlreadyExistsException(String message, Throwable ex) {
+        super(message, ex);
+    }
+
+    public EntryAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
 }
