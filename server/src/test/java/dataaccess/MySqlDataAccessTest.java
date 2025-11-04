5ff6969d8d9402760459c278cfb3755f17aeb7d7
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-11-03 18:49:29 -0700

    Create MySqlDataAccessTest boilerplate

---
+++ server/src/test/java/dataaccess/MySqlDataAccessTest.java
@@ -0,0 +1,67 @@
+package dataaccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySqlDataAccessTest {
+    private MySqlDataAccess mySqlDataAccess;
+
+    @BeforeEach
+    void beforeEach() throws DataAccessException {
+        mySqlDataAccess = new MySqlDataAccess();
+    }
+
+    @Test
+    void clearUsers() {
+    }
+
+    @Test
+    void clearAuthData() {
+    }
+
+    @Test
+    void clearGameData() {
+    }
+
+    @Test
+    void insertUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void insertAuthData() {
+    }
+
+    @Test
+    void getAuthData() {
+    }
+
+    @Test
+    void removeAuth() {
+    }
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void getUserFromAuth() {
+    }
+
+    @Test
+    void getGame() {
+    }
+
+    @Test
+    void updateGame() {
+    }
+}
