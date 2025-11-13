d10d1726c80252f5743048eebb60b08c70e7649a
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-11-12 21:33:32 -0700

    Create ResponseException#fromJson method for parsing error responses

--- client/src/main/java/server/ResponseException.java
+++ client/src/main/java/server/ResponseException.java
@@ -1,5 +1,9 @@
 package server;
 
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+
 public class ResponseException extends RuntimeException {
     public ResponseException(String message) {
         super(message);
@@ -7,5 +11,10 @@ public class ResponseException extends RuntimeException {
 
     public ResponseException(Throwable cause) {
         super(cause);
+    }
+
+    public static ResponseException fromJson(String json) {
+        var body = new Gson().fromJson(json, HashMap.class);
+        return new ResponseException(body.get("message").toString());
     }
 }
