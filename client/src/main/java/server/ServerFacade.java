1de833151d24b963cb8ecb863450fe382bfaf400
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-11-12 18:06:03 -0700

    Create boilerplate for ServerFacade

---
+++ client/src/main/java/server/ServerFacade.java
@@ -0,0 +1,29 @@
+package server;
+
+public class ServerFacade {
+
+    public void register() {
+        // TODO
+    }
+
+    public void login() {
+        // TODO
+    }
+
+    public void logout() {
+        // TODO
+    }
+
+    public void listGames() {
+        // TODO
+    }
+
+    public void createGame() {
+        // TODO
+    }
+
+    public void joinGame() {
+        // TODO
+    }
+
+}
