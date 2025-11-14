50b8d15a1ac7c9b8ba7267a3c437415c1cf55b32
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-11-13 22:16:50 -0700

    Add basic functionality to ChessClient#evaluate and create skeleton code for all commands

--- client/src/main/java/ChessClient.java
+++ client/src/main/java/ChessClient.java
@@ -1,6 +1,11 @@
+import server.ResponseException;
 import server.ServerFacade;
+import ui.EscapeSequences;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
 
 public class ChessClient {
     private final ServerFacade server;
@@ -26,15 +31,81 @@ public class ChessClient {
             try {
                 result = evaluate(input);
                 System.out.println(result);
-            } catch (Throwable ex) {
-                System.out.printf("Error: %s%n", ex.getMessage());
+            } catch (Throwable e) {
+                System.out.printf("Error: %s%n", e.getMessage());
             }
         }
     }
 
     private String evaluate(String input) {
+        String[] tokens = input.split(" ");
+        String cmd = (tokens.length > 0) ? tokens[0].toLowerCase() : "help";
+        String[] args = Arrays.copyOfRange(tokens, 1, tokens.length);
+
+        try {
+            return switch (cmd) {
+                // PreLogin
+                case "quit" -> quit();
+                case "login" -> login(args);
+                case "register" -> register(args);
+
+                // PostLogin
+                case "logout" -> logout(args);
+                case "create-game" -> createGame(args);
+                case "list-games" -> listGames(args);
+                case "play" -> playGame(args);
+                case "observe" -> observeGame(args);
+
+                default -> help();
+            };
+        } catch (ResponseException e) {
+            return SET_TEXT_COLOR_RED + "Error: " + e.getMessage() + RESET_TEXT_COLOR;
+        }
+    }
+
+    private String help() throws ResponseException {
         // TODO
-        return "Not implemented yet!";
+        throw new ResponseException("Not implemented yet!");
+    }
+
+    private String quit() throws ResponseException {
+        // TODO
+        throw new ResponseException("Not implemented yet!");
+    }
+
+    private String login(String[] args) throws ResponseException {
+        // TODO
+        throw new ResponseException("Not implemented yet!");
+    }
+
+    private String register(String[] args) throws ResponseException {
+        // TODO
+        throw new ResponseException("Not implemented yet!");
+    }
+
+    private String logout(String[] args) throws ResponseException {
+        // TODO
+        throw new ResponseException("Not implemented yet!");
+    }
+
+    private String createGame(String[] args) throws ResponseException {
+        // TODO
+        throw new ResponseException("Not implemented yet!");
+    }
+
+    private String listGames(String[] args) throws ResponseException {
+        // TODO
+        throw new ResponseException("Not implemented yet!");
+    }
+
+    private String playGame(String[] args) throws ResponseException {
+        // TODO
+        throw new ResponseException("Not implemented yet!");
+    }
+
+    private String observeGame(String[] args) throws ResponseException {
+        // TODO
+        throw new ResponseException("Not implemented yet!");
     }
 
     private String promptInput(Scanner scanner) {
