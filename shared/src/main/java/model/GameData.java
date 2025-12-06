77ace2dd2eb620a76bb457a60c018b2c2c612793
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-12-05 21:23:02 -0700

    Add GameData#getTeamOfPlayer to get team color of given user

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,9 +1,30 @@
 package model;
 
 import chess.ChessGame;
+import chess.ChessGame.TeamColor;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
     public GameData(int gameID, String gameName) {
         this(gameID, null, null, gameName, new ChessGame());
+    }
+
+    /**
+     * Get the team color of a user in the game.
+     *
+     * @param username The name of the user.
+     * @return WHITE if the user is on the White team; BLACK if the user is on the Black team; null otherwise.
+     */
+    public TeamColor getTeamOfPlayer(String username) {
+        TeamColor teamColor;
+
+        if (whiteUsername.equals(username)) {
+            teamColor = TeamColor.WHITE;
+        } else if (blackUsername.equals(username)) {
+            teamColor = TeamColor.BLACK;
+        } else {
+            teamColor = null;
+        }
+
+        return teamColor;
     }
 }
