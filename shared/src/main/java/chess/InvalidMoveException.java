ef4828535df0cd765c82da91e24730124ce2a3a5
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-12-05 22:22:15 -0700

    Update InvalidMoveException to optionally include reason for invalid move (INVALID or GAME_ENDED).

--- shared/src/main/java/chess/InvalidMoveException.java
+++ shared/src/main/java/chess/InvalidMoveException.java
@@ -4,10 +4,22 @@ package chess;
  * Indicates an invalid move was made in a game
  */
 public class InvalidMoveException extends Exception {
+    public enum InvalidMoveReason {
+        INVALID, GAME_ENDED
+    }
 
-    public InvalidMoveException() {}
+    private InvalidMoveReason reason;
 
     public InvalidMoveException(String message) {
+        this(message, InvalidMoveReason.INVALID);
+    }
+
+    public InvalidMoveException(String message, InvalidMoveReason reason) {
         super(message);
+        this.reason = reason;
+    }
+
+    public InvalidMoveReason getReason() {
+        return reason;
     }
 }
