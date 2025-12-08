a806f99a9db50c7906f8d320b7a4334481c62f9c
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-12-08 15:22:58 -0700

    Create ChessMoveInterpreter to convert chess coordinate strings (e.g. "A4") to ChessPosition objects

---
+++ client/src/main/java/client/ChessMoveInterpreter.java
@@ -0,0 +1,59 @@
+package client;
+
+import chess.ChessPiece.PieceType;
+import chess.ChessPosition;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+
+public class ChessMoveInterpreter {
+    private final Map<Character, PieceType> promotionMap = Map.of(
+            'q', PieceType.QUEEN,
+            'r', PieceType.ROOK,
+            'b', PieceType.BISHOP,
+            'k', PieceType.KNIGHT
+    );
+
+    public ChessPosition positionFromString(String posString) {
+        if (!validatePositionString(posString)) {
+            throw new IllegalArgumentException("Chess move string should be of the format `A1` or `a1`");
+        }
+
+        char colChar = Character.toLowerCase(posString.charAt(0));
+        int col = colChar - 'a' + 1;
+        int row = posString.charAt(1) - '0';
+        return new ChessPosition(row, col);
+    }
+
+    public PieceType pieceTypeFromString(String pieceString) {
+        PieceType result;
+        try {
+            result = promotionMap.get(pieceString.charAt(0));
+            Assertions.assertNotNull(result);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    "Input should be a char representing a valid promotion piece (" + promotionMapDetails() + ")"
+            );
+        }
+        return result;
+    }
+
+    private String promotionMapDetails() {
+        Collection<String> promotions = new HashSet<>();
+        promotionMap.forEach((c, piece) -> promotions.add("'" + c + "' - " + piece));
+        return String.join(", ", promotions);
+    }
+
+    public boolean validatePositionString(String posString) {
+        if (posString.length() < 2) {
+            return false;
+        }
+        if (!Character.isLetter(posString.charAt(0))) {
+            return false;
+        }
+
+        return Character.isDigit(posString.charAt(1));
+    }
+}
